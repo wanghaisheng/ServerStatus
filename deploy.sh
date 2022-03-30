cd3ff3a6565e14f9e4f8cb2180d25cd1af5374b5
literature <65801567+LiteraturePro@users.noreply.github.com>
2022-03-30 20:46:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,47 @@ if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve App")" = "Could not re
 else
     printf '\e[33mThe app has been created.\n\e[0m'
 fi
-
+printf '\e[33mNext, create app config file - fly.toml.\n\e[0m'
+cat <<EOF >./fly.toml
+app = "$APP_NAME"
+kill_signal = "SIGINT"
+kill_timeout = 5
+processes = []
+[env]
+[experimental]
+  allowed_public_ports = []
+  auto_rollback = true
+[[services]]
+  http_checks = []
+  internal_port = 443
+  # processes = ["app"]
+  protocol = "tcp"
+  script_checks = []
+  [services.concurrency]
+    hard_limit = 50
+    soft_limit = 35
+    type = "connections"
+  [[services.ports]]
+    handlers = ["http"]
+    port = 80
+  [[services.ports]]
+    handlers = ["tls", "http"]
+    port = 443
+  [[services.tcp_checks]]
+    grace_period = "120s"
+    interval = "15s"
+    restart_limit = 0
+    timeout = "2s"
+[[services]]
+  internal_port = 35601
+  protocol = "tcp"
+  [services.concurrency]
+    hard_limit = 25
+    soft_limit = 20
+  [[services.ports]]
+    handlers = []
+    port = "5000"
+EOF
 printf '\e[32mCreate app config file success.\n\e[0m'
 printf '\e[33mNext, set app secrets and regions.\n\e[0m'
 
